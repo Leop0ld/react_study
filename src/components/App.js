13e4a2fc8e7b6f08996d11b470ead31ff8815628
Leop0ld <l3opold7@gmail.com>
2016-09-07 00:35:59 +0900

    Inline style & 삼항연산자 추가

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,20 @@ import React from 'react';
 
 class App extends React.Component {
     render(){
+		let description = 'Welcome, Leop0ld!';
 
-        return (
-                <h1>Hello, React!</h1>
-        );
+		let leoStyle = {
+			color: '#fff',
+			backgroundColor: '#000',
+			border: '10px solid pink',
+			borderRadius: '10px'
+		}
+		return (
+			<div>
+				<h1 style={leoStyle}>Hello, Leop0ld!</h1>
+				<p>{1 != 2 ? 'Leop0ld' : 'False'}</p>
+			</div>
+		);
     }
 }
 
